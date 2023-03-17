137c07bb340fa2edb5ceafd97dbde5ed154be4fb
rashed <x.suwaidi@gmail.com>
2023-03-17 11:57:46 +0400

    ui update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,76 +1,196 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { IconContext, IconType } from 'react-icons';
+import { MdLocalMovies } from "react-icons/md";
+import { AiFillEyeInvisible } from "react-icons/ai";
+import { BsFillMicMuteFill } from "react-icons/bs";
+import { GiFist } from "react-icons/gi";
+import { RiNetflixFill } from "react-icons/ri";
+import { FaSyringe, FaStarAndCrescent } from "react-icons/fa";
 import { Background, Parallax } from 'react-parallax';
 
-const Home: NextPage = () => {
-  return (
-    <div className="flex bg-slate-900 min-h-screen flex-col  justify-center ">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#0f172a" />
-      </Head>
-
-
-      {/* parallax background  */}
-      <Parallax strength={199}>
-        <Background className="w-screen h-screen bg-cover">
-          <div className='w-full h-full relative'>
-
-            <img className='object-cover w-full h-full' src="https://static.invenglobal.com/upload/image/2021/10/13/i1634084905769735.jpeg" alt="fill murray" />
-
-
-          </div>
-        </Background>
-
-        <div className=' w-screen min-h-screen text-center'>
-          <div className='bg-black  w-screen bg-opacity-60 py-4 inset-x-0 bottom-0 absolute'>
-            <p className=' text-white text-6xl font-bold'>Arcane</p>
-            <div className=' flex text-white justify-center mt-4 gap-4'>
-              <p className=' bg-yellow-300 bg-opacity-30 rounded-md px-2 text-sm backdrop-blur-sm'>12+</p>
-              <p className=' bg-yellow-300 bg-opacity-30 rounded-md px-2 text-sm'>PG-15</p>
-            </div>
-
-            <div className=' h-28'>
-              <div className='icon-scroll scale-75'></div>
-            </div>
-          </div>
-        </div>
-      </Parallax>
-
-
-      <main className="flex flex-1 flex-col mt-12 mx-4 ">
-
-        <div className=' w-full  '>
-
-          <div className="w-full bg-gray-200  h-2.5 dark:bg-gray-700">
-            <div className="bg-blue-600 h-2.5 " style={{ width: "45%" }}></div>
-          </div>
-
-        </div>
-
-        {/* <p className=' text-white font-bold '> Movie</p> */}
-
-        <div className=' text-start max-w-2xl mt-4'>
-          <p className=' text-white font-bold text-xl'>Title</p>
-          <p className=' text-slate-500 text-sm bg-slate-800  py-4 rounded mt-2 px-1'>The foreground color of a card depends on the background color you choose for the card. Generally, for a dark-themed app, you would use a lighter color for the foreground so that the text and other elements are easily visible against the dark background. For example, if you choose a dark blue color for the background of the card, you could use white or off-white color for the foreground elements such as text, icons, etc. Alternatively, you could use a light gray color for the foreground to create a softer contrast. Ultimately, the choice of foreground color depends on the specific color palette you choose and the overall design aesthetic you want to achieve.</p>
-        </div>
-
-        <div className=' text-start  max-w-2xl mt-4'>
-          <p className=' text-white font-bold text-xl'>Title</p>
-          <p className=' text-slate-500 text-sm bg-slate-800 py-4 rounded mt-2 px-1'>The foreground color of a card depends on the background color you choose for the card. Generally, for a dark-themed app, you would use a lighter color for the foreground so that the text and other elements are easily visible against the dark background. For example, if you choose a dark blue color for the background of the card, you could use white or off-white color for the foreground elements such as text, icons, etc. Alternatively, you could use a light gray color for the foreground to create a softer contrast. Ultimately, the choice of foreground color depends on the specific color palette you choose and the overall design aesthetic you want to achieve.</p>
-        </div>
-
-
-
-
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center">
-      </footer>
-    </div>
-  )
+type indicator = {
+  percentage: number,
+  title: string
 }
+
+const indicator_var: indicator[] = [
+  {
+    percentage: 24,
+    title: 'Violence'
+  },
+  {
+    percentage: 36,
+    title: 'Language'
+  },
+  {
+    percentage: 87,
+    title: 'Inappropriate'
+  },
+  {
+    percentage: 92,
+    title: 'Overall'
+  },
+
+]
+
+interface PrimaryIconProps {
+  children: React.ReactNode;
+}
+
+
+function PrimaryIcon({ children }: PrimaryIconProps) {
+  return (
+    <IconContext.Provider value={{ size: '24px', className: 'text-white' }}>
+      <div className=' bg-teal-500 w-fit rounded p-1'>
+
+        <div>{children}</div>
+      </div>
+    </IconContext.Provider>
+  );
+}
+
+
+
+const Home: NextPage = () => (
+  <div className="flex bg-slate-900 min-h-screen flex-col justify-center ">
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="theme-color" content="#0f172a" />
+    </Head>
+
+
+    {/* parallax background  */}
+    <Parallax strength={199}>
+      <Background className="w-screen h-screen bg-cover">
+        <div className='w-full h-full relative'>
+
+          <img className='object-cover w-full h-full' src="https://static.invenglobal.com/upload/image/2021/10/13/i1634084905769735.jpeg" alt="fill murray" />
+
+
+        </div>
+      </Background>
+
+      <div className=' w-screen min-h-screen text-center'>
+
+        <div className='bg-black  w-screen bg-opacity-60 py-4 inset-x-0 bottom-0 absolute'>
+          <p className=' text-white text-6xl font-bold'>Arcane</p>
+          <div className=' flex text-white justify-center mt-4 gap-4'>
+            <p className=' bg-yellow-300 bg-opacity-30 rounded-md px-2 text-sm backdrop-blur-sm'>12+</p>
+            <p className=' bg-yellow-300 bg-opacity-30 rounded-md px-2 text-sm'>PG-15</p>
+          </div>
+
+          <div className=' h-28'>
+            <div className='icon-scroll scale-75'></div>
+          </div>
+        </div>
+      </div>
+    </Parallax>
+
+
+    <main className="flex flex-1 flex-col mt-6 mx-4 ">
+      <div className=' mx-auto max-w-2xl'>
+
+        <div className=' flex gap-2'>
+
+          <p className=' text-white '>Series </p>
+          <div className='my-auto text-red-500'>
+            <RiNetflixFill />
+          </div>
+        </div>
+        <p className=' text-slate-500 text-xs'>Duration: 2h 30min</p>
+
+        <div className=' grid grid-cols-2 gap-2 mt-4'>
+
+          {indicator_var.map((indicator) => (
+            <div className=' bg-slate-800 rounded p-2'>
+              <p className=' text-slate-500 text-xs'>{indicator.title}</p>
+              <div className=' flex gap-2'>
+
+                <p className=' text-white text-lg'>{indicator.percentage}</p>
+
+                <div className=' w-full my-auto'>
+                  <div className="w-full bg-gray-200 rounded h-1.5 dark:bg-gray-700">
+                    <div className="bg-teal-500 rounded h-1.5 " style={{ width: indicator.percentage.toString() + '%' }}></div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className=' text-start mt-4 px-4 py-6 bg-slate-800 rounded'>
+          <PrimaryIcon>
+            <MdLocalMovies />
+          </PrimaryIcon>
+          <p className=' text-white text-xl mt-2'>Summary</p>
+          <article className=' text-slate-500 text-sm mt-2 prose prose-invert'>
+            <p>"The Arcane" is an animated TV series set in the fictional utopian city of Piltover and the underground criminal world of Zaun, inspired by the "League of Legends" universe. The series follows two sisters, Vi and Jinx, who grew up in the impoverished underworld of Zaun and were separated due to an incident that occurred in their youth.</p>
+          </article>
+        </div>
+
+        <div className=' text-start mt-4 px-4 py-6 bg-slate-800 rounded'>
+          <PrimaryIcon>
+            <AiFillEyeInvisible />
+          </PrimaryIcon>
+          <p className=' text-white text-xl mt-2'>Inappropriate</p>
+          <article className=' text-slate-500 text-sm mt-2 prose prose-invert'>
+            <p>"The Arcane" is an animated TV series set in the fictional utopian city of Piltover and the underground criminal world of Zaun, inspired by the "League of Legends" universe. The series follows two sisters, Vi and Jinx, who grew up in the impoverished underworld of Zaun and were separated due to an incident that occurred in their youth.</p>
+          </article>
+        </div>
+
+
+        <div className=' text-start mt-4 px-4 py-6 bg-slate-800 rounded'>
+          <PrimaryIcon>
+            <BsFillMicMuteFill />
+          </PrimaryIcon>
+          <p className=' text-white text-xl mt-2'>Language</p>
+          <article className=' text-slate-500 text-sm mt-2 prose prose-invert'>
+            <p>"The Arcane" is an animated TV series set in the fictional utopian city of Piltover and the underground criminal world of Zaun, inspired by the "League of Legends" universe. The series follows two sisters, Vi and Jinx, who grew up in the impoverished underworld of Zaun and were separated due to an incident that occurred in their youth.</p>
+          </article>
+        </div>
+
+        <div className=' text-start mt-4 px-4 py-6 bg-slate-800 rounded'>
+          <PrimaryIcon>
+            <GiFist />
+          </PrimaryIcon>
+          <p className=' text-white text-xl mt-2'>Violence</p>
+          <article className=' text-slate-500 text-sm mt-2 prose prose-invert'>
+            <p>"The Arcane" is an animated TV series set in the fictional utopian city of Piltover and the underground criminal world of Zaun, inspired by the "League of Legends" universe. The series follows two sisters, Vi and Jinx, who grew up in the impoverished underworld of Zaun and were separated due to an incident that occurred in their youth.</p>
+          </article>
+        </div>
+
+
+        <div className=' text-start mt-4 px-4 py-6 bg-slate-800 rounded'>
+          <PrimaryIcon>
+            <FaSyringe />
+          </PrimaryIcon>
+          <p className=' text-white text-xl mt-2'>Drugs</p>
+          <article className=' text-slate-500 text-sm mt-2 prose prose-invert'>
+            <p>"The Arcane" is an animated TV series set in the fictional utopian city of Piltover and the underground criminal world of Zaun, inspired by the "League of Legends" universe. The series follows two sisters, Vi and Jinx, who grew up in the impoverished underworld of Zaun and were separated due to an incident that occurred in their youth.</p>
+          </article>
+        </div>
+
+
+        <div className=' text-start mt-4 px-4 py-6 bg-slate-800 rounded'>
+          <PrimaryIcon>
+            <FaStarAndCrescent />
+          </PrimaryIcon>
+          <p className=' text-white text-xl mt-2'>Religion</p>
+          <article className=' text-slate-500 text-sm mt-2 prose prose-invert'>
+            <p>"The Arcane" is an animated TV series set in the fictional utopian city of Piltover and the underground criminal world of Zaun, inspired by the "League of Legends" universe. The series follows two sisters, Vi and Jinx, who grew up in the impoverished underworld of Zaun and were separated due to an incident that occurred in their youth.</p>
+          </article>
+        </div>
+
+
+      </div>
+    </main>
+
+    <footer className="flex h-24 w-full items-center justify-center">
+    </footer>
+  </div>
+)
 
 export default Home
