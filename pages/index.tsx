5b6ee0a1eaf4277b109650dc8606baca62ec4109
rashed <x.suwaidi@gmail.com>
2023-03-19 09:50:17 +0400

    fix error with head

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,7 @@ const MovieList: NextPage = () => {
             <meta property="telegram:title" content="قائمة الأفلام | الآن تعرف" />
             <meta property="telegram:description" content="الآن تعرف - اكتشف ما إذا كان الفيلم مناسبًا للأطفال." />
             <meta property="telegram:image" content="https://images.unsplash.com/photo-1595425178239-7f2e3af36041?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
-          </Head
+          </Head>
 
 
 
