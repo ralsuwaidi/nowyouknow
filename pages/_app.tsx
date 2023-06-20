33340ee99c0b62bae7aa9ce37b1ffe17f9600e60
Rashed AlSuwaidi <rashed.alsuwaidi@ai.gov.ae>
2023-06-20 15:10:15 +0400

    remove font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
-    <style jsx global>{`
-        html {
-          font-family: ${inter.style.fontFamily};
-        }
-      `}</style>
     <Component {...pageProps} />
   </>
 
