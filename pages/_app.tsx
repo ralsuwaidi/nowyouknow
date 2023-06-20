c06bc19fc077c6d1cf7ecb0b8fb97d6138a16968
Rashed AlSuwaidi <rashed.alsuwaidi@ai.gov.ae>
2023-06-20 15:23:24 +0400

    change font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Noto_Sans_Arabic } from 'next/font/google'
 
+const noto = Noto_Sans_Arabic({ subsets: ['arabic'] })
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
+    <style jsx global>{`
+        html {
+          font-family: ${noto.style.fontFamily};
+        }
+      `}</style>
     <Component {...pageProps} />
   </>
 
