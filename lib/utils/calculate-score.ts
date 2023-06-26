4cafa2a336f312d13df2460e42899c2a8303a7c7
Rashed AlSuwaidi <rashed.alsuwaidi@ai.gov.ae>
2023-06-26 12:23:08 +0400

    fix percentage calculation

--- lib/utils/calculate-score.ts
+++ lib/utils/calculate-score.ts
@@ -1,9 +1,9 @@
 import { MovieDataType } from "./movie-type";
 
 export const CalculateOverall = (movie: MovieDataType): number => {
-  const violence_rating = movie.attributes.violence_rating;
-  const inappropriate_rating = movie.attributes.inappropriate_rating;
-  const language_rating = movie.attributes.language_rating;
+  const violence_rating = GetPercentage(movie.attributes.violence_rating);
+  const inappropriate_rating = GetPercentage(movie.attributes.inappropriate_rating);
+  const language_rating = GetPercentage(movie.attributes.language_rating);
 
   const total = violence_rating + inappropriate_rating + language_rating;
 
